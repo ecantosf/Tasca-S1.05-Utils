517e70cb261e3189eb89ceceb644eb9deb6bfa8f
fonca <eduard.cantos@gmail.com>
2026-01-02 00:47:59 +0100

    feat: complete Level 1 Exercise 1 - alphabetical directory listing
    Implement DirectoryLister class for Level 1 Exercise 1 requirement:
    - Lists directory contents alphabetically from command-line parameter
    - Supports execution via: java DirectoryLister <directory_path>

--- Level1/basic-utilities-s1-t5-l1/src/utilities/basic/DirectoryLister.java
+++ Level1/basic-utilities-s1-t5-l1/src/utilities/basic/DirectoryLister.java
@@ -1,7 +1,42 @@
 package utilities.basic;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class DirectoryLister {
     // Exercici 1: Llistar contingut alfabètic
     // Exercici 2: Llistar arbre directoris amb D/F i data
     // Exercici 3: Guardar resultat en fitxer TXT
+
+    public static void listDirectoryAlphabetically(String directoryPath) {
+        File directory = new File(directoryPath);
+
+        if (!directory.exists()) {
+            System.err.println("Error: Directory " + directoryPath + " does not exist.");
+            return;
+        }
+
+        if(!directory.isDirectory()) {
+            System.err.println("Error: Directory " + directoryPath + " is not directory.");
+            return;
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null || files.length == 0) {
+            System.err.println("Error: Directory " + directoryPath + " is empty.");
+            return;
+        }
+
+        assert files != null;
+        Arrays.sort(files, Comparator.comparing(File::getName));
+
+        System.out.println("Directory contents: " + directoryPath);
+
+        for (File file : files) {
+            String type = file.isDirectory() ? "[Directory]" : "[File]";
+            System.out.println("\t" + type + " " + file.getName());
+        }
+
+    }
 }
--- Level1/basic-utilities-s1-t5-l1/src/utilities/basic/Main.java
+++ Level1/basic-utilities-s1-t5-l1/src/utilities/basic/Main.java
@@ -2,6 +2,12 @@ package utilities.basic;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Project created properly!");
+        if (args.length > 0) {
+            String directoryPath = args[0];
+            DirectoryLister.listDirectoryAlphabetically(directoryPath);
+        } else {
+            String defaultDirectoryPath = "test-directory";
+            DirectoryLister.listDirectoryAlphabetically(defaultDirectoryPath);
+        }
     }
 }
