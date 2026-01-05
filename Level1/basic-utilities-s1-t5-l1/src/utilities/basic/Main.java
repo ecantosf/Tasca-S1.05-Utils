ec01a07b9de714d75d92236d0fdcd15c6845a500
fonca <eduard.cantos@gmail.com>
2026-01-05 18:49:44 +0100

    feat: complete exercise 4 for reading .txt file and console output

--- Level1/basic-utilities-s1-t5-l1/src/utilities/basic/Main.java
+++ Level1/basic-utilities-s1-t5-l1/src/utilities/basic/Main.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class Main {
 
     private static final String OUTPUT_PATH = "C:\\Users\\fonca\\IdeaProjects\\Sprint1\\T5_Utils\\Level1\\basic-utilities-s1-t5-l1\\test-directory\\directory_tree_output.txt";
+    private static Scanner scanner;
 
     public static void main(String[] args) {
 
+        scanner = new Scanner(System.in);
         cleanOutputFile();
 
         if (args.length > 0) {
@@ -25,12 +27,14 @@ public class Main {
         System.out.println("1. Alphabetically (Exercise 1)");
         System.out.println("2. Directory Tree (Exercise 2)");
         System.out.println("3. Save Directory Tree to TXT file (Exercise 3)");
-        System.out.print("Enter your choice (1, 2 or 3): ");
+        System.out.println("4. Read and Show from TXT file (Exercise 4)");
+        System.out.print("Enter your choice (1, 2, 3 or 4): ");
 
         Scanner scanner = new Scanner(System.in);
 
         try {
             int choice = scanner.nextInt();
+            scanner.nextLine();
 
             String directory = "test-directory";
             String outputPath = OUTPUT_PATH;
@@ -44,9 +48,11 @@ public class Main {
             } else if (choice == 3) {
                 System.out.println("\nExercise 3: Save Directory Tree to TXT File");
                 System.out.println("Saving directory tree to: " + outputPath);
-
                 DirectoryLister.saveDirectoryTreeToFile(directory, outputPath);
-            } else {
+            } else if (choice == 4) {
+                System.out.println("\nExercise 4: Read and Show TXT File");
+                ReadAndWriteFile();
+            }else {
                 System.out.println("\nInvalid choice! Defaulting to Exercise 1.");
                 System.out.println("Exercise 1: Alphabetical List");
                 DirectoryLister.listDirectoryAlphabetically(directory);
@@ -56,8 +62,6 @@ public class Main {
             System.err.println("Error: Please enter a valid number (1 or 2)");
             System.err.println("Using default Exercise 1...");
             DirectoryLister.listDirectoryAlphabetically("test-directory");
-        } finally {
-            scanner.close();
         }
     }
 
@@ -72,4 +76,15 @@ public class Main {
             }
         }
     }
+
+    private static void ReadAndWriteFile() {
+        System.out.print("File Path: ");
+        String filePath = scanner.nextLine();
+        System.out.print("File Name: ");
+        String fileName = scanner.nextLine();
+
+        TXTFileReader fileReader = new TXTFileReader();
+        fileReader.readAndShow(filePath, fileName);
+    }
+
 }
--- Level1/basic-utilities-s1-t5-l1/src/utilities/basic/TXTFileReader.java
+++ Level1/basic-utilities-s1-t5-l1/src/utilities/basic/TXTFileReader.java
@@ -1,5 +1,25 @@
 package utilities.basic;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class TXTFileReader {
-    // Exercici 4: Llegir i mostrar contingut TXT
-}
+
+    public void readAndShow(String filePath, String fileName) {
+        Path pathSearched = Paths.get(filePath, fileName);
+
+        try (BufferedReader br = Files.newBufferedReader(pathSearched)){
+            String line;
+            while ((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            System.out.println("Error reading file " + e.getMessage());
+        }
+    }
+
+    }
